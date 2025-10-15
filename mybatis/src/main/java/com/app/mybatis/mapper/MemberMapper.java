f29906e01ed1c482260b83fe1b5fe59faf63cced
CWB\fpqk3 <wbu3us2@gmail.com>
2025-10-15 17:07:37 +0900

    1015-1707

--- mybatis/src/main/java/com/app/mybatis/mapper/MemberMapper.java
+++ mybatis/src/main/java/com/app/mybatis/mapper/MemberMapper.java
@@ -4,11 +4,12 @@ import com.app.mybatis.domain.MemberVO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.lang.reflect.Member;
+import java.util.Optional;
 
 @Mapper
 public interface MemberMapper {
     public void insert(MemberVO memberVO);
-    public MemberVO selectOne(MemberVO memberVO);
+    public Optional<MemberVO> selectOne(MemberVO memberVO);
     public void update(MemberVO memberVO);
-    public void delete(String memberId);
+    public void delete(String id);
 }
--- mybatis/src/test/java/com/app/mybatis/mapper/MemberMapperTests.java
+++ mybatis/src/test/java/com/app/mybatis/mapper/MemberMapperTests.java
@@ -1,6 +1,7 @@
 package com.app.mybatis.mapper;
 
 import com.app.mybatis.domain.MemberVO;
+import com.app.mybatis.domain.ProductVO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class MemberMapperTests {
     @Autowired
     private MemberMapper memberMapper;
+    @Autowired
+    private ProductMapper productMapper;
 
     @Test
     public void test() {
@@ -20,7 +23,6 @@ public class MemberMapperTests {
         memberVO.setMemberEmail("fpqk33@naver.com");
         memberMapper.insert(memberVO);
     }
-
     @Test
     public void testSelectOne() {
 
@@ -28,8 +30,60 @@ public class MemberMapperTests {
 
         inputMemberVO.setMemberEmail("fpqk33@naver.com");
         inputMemberVO.setMemberPassword("1234");
-
-        MemberVO memberVO = memberMapper.selectOne(inputMemberVO);
-        log.info("memberVO: {}", memberVO);
+        memberMapper.selectOne(inputMemberVO).ifPresent(resultVO -> {
+            log.info("memberVO: {}", resultVO);
+        });
     }
+
+    @Test
+    public void testUpdate() {
+        MemberVO inputMember = new MemberVO();
+        inputMember.setMemberName("보자기");
+        inputMember.setMemberPassword("3424");
+        inputMember.setMemberEmail("ourneko033@gmail.com");
+        inputMember.setId(1L);
+
+        memberMapper.update(inputMember);
+    }
+
+    @Test
+    public void testDelete() {
+        memberMapper.delete("1");
+    }
+
+    @Test
+    public void testProductInsert(){
+        ProductVO productVO = new ProductVO();
+        productVO.setProductName("vovo");
+        productVO.setProductPrice(50000);
+        productVO.setProductStock(50);
+
+        productMapper.insert(productVO);
+    }
+    @Test
+    public void testProductSelectOne(){
+        ProductVO productVO = productMapper.selectOne(1L);
+        log.info("productVO: {}", productVO);
+    }
+
+    @Test
+    public void testProductSelectAll(){
+        productMapper.selectAll().forEach(productVO -> log.info("productVO: {}", productVO));
+    }
+
+    @Test
+    public void testProductUpdate(){
+        ProductVO productVO = new ProductVO();
+        productVO.setId(1L);
+        productVO.setProductName("vovo2");
+        productVO.setProductPrice(50000);
+        productVO.setProductStock(50);
+
+        productMapper.update(productVO);
+    }
+    @Test
+    public void testProductDelete(){
+        productMapper.delete(1L);
+    }
+
 }
